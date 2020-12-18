ac3bd3fc46199033d01628aa0f2dcc687330c1a3
delayedaa <delayedaa@gmail.com>
2020-12-18 14:39:20 -0600

    [App.js] Add nowCurrentUser and handleLogout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Components, useEffect, useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
 
 import Welcome from './components/Welcome';
 import Nav from './components/Nav';
@@ -20,8 +21,37 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 };
 
 function App() {
+  const [currentUser, setCurrentUser] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  useEffect(() => {
+    let token;
+    if (!localStorage.getItem('jwtToken')) {
+      setIsAuthenticated(false);
+    } else {
+      token = jwtDecode(localStorage.getItem('jwtToken'));
+      setAuthToken(localStorage.jwtToken);
+      setCurrentUser(token);
+    }
+  }, []);
+
+  const nowCurrentUser = (userData) => {
+    console.log('nowCurrentUser is here.');
+    setCurrentUser(userData);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    if (localStorage.getItem('jwtToken')) {
+      localStorage.removeItem('jwtToken');
+      setCurrentUser('');
+      setIsAuthenticated(false);
+    }
+  };
+
   return (
     <div className="App">
+      <Nav handleLogout={handleLogout} isAuth={isAuthenticated} />
       <Welcome />
     </div>
   );
