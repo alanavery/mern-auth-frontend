73a22b9547f1801be972d1f6f97d05de6800898e
delayedaa <delayedaa@gmail.com>
2020-12-18 14:15:11 -0600

    [App.js] Add PrivateRoute function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React, { Components, useEffect, useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+
 import Welcome from './components/Welcome';
+import Nav from './components/Nav';
+
 import './App.css';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const user = localStorage.getItem('jwtToken');
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return user ? <Component {...rest} {...props} /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+};
 
 function App() {
   return (
