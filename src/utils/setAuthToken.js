9ff53c8232f75c124002705ddae0cb4289c2f0dc
delayedaa <delayedaa@gmail.com>
2020-12-18 10:56:03 -0600

    [setAuthToken.js] Add setAuthToken function

---
+++ src/utils/setAuthToken.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const setAuthToken = (token) => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+    console.log('Headers ——————————');
+    console.log(axios.defaults.headers.common);
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+export default setAuthToken;
